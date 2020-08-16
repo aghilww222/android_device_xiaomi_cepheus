bb78527d5ed7645b965820875d97c32c6af05752
daniml3 <danimoral1001@gmail.com>
2020-08-16 18:26:54 +0200

    cepheus: adapt FOD to ArrowOS
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- device.mk
+++ device.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Fstab
 PRODUCT_COPY_FILES += \
