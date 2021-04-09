a5089e5f1a25c6061fb7071f7315a3c0f114b160
lamjiidii1 <lamjiidii1@gmail.com>
2021-04-09 17:09:41 +0100

    cepheus: track and build Google Camera GO

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
+# Camera GO
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
+    
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
