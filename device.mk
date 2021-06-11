2dfa3d13cdec871eb0e8c92cb7259f3a1f072a09
Savsir <svsrcnkt@gmail.com>
2021-06-11 23:42:47 +0300

    Revert "cepheus: Switch to QTI thermal HAL"
    This reverts commit b4aae242819ad439c2b4d16812811ed1d36c52b8.

--- device.mk
+++ device.mk
@@ -438,7 +438,11 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+PRODUCT_PACKAGES += \
+    thermal.msmnile
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
