55a10ec93e80424e71227311370629c08193b23f
Savsir <svsrcnkt@gmail.com>
2021-06-11 23:40:13 +0300

    Revert "cepheus: Switch to pixel thermal hal"
    This reverts commit 3c51693d1062c0f0cb46f3a6ce800d58ecdc35c5.

--- device.mk
+++ device.mk
@@ -438,8 +438,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.cepheus \
-    thermal_logd
+    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
