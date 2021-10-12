b135b8f0c4e8484eb4a03e4e147a06fbdd566755
Lamjidi Othman <lamjiidii1@gmail.com>
2021-10-12 14:39:15 +0000

    cepheus: Drop FOD stuff

--- device.mk
+++ device.mk
@@ -184,11 +184,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Fstab
 PRODUCT_COPY_FILES += \
