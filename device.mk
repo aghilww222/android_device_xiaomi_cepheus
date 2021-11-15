22bf4fa08062bb2f3ac4a3475f8723e88d308524
daniml3 <daniel@danielml.dev>
2021-11-15 14:05:57 +0000

    cepheus: build the GPS/GNSS stack from source

--- device.mk
+++ device.mk
@@ -197,14 +197,22 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1-impl-qti \
-    android.hardware.gnss@2.1-service-qti
+     android.hardware.gnss@2.1-impl-qti \
+     android.hardware.gnss@2.1-service-qti
 
 PRODUCT_PACKAGES += \
-    libbatching \
-    libgeofencing \
-    libgnss \
-    libsensorndkbridge
+     gps.conf \
+     flp.conf \
+     gnss_antenna_info.conf \
+     libloc_pla_headers \
+     liblocation_api_headers \
+     libgps.utils_headers \
+     liblocation_api \
+     libgps.utils \
+     libbatching \
+     libgeofencing \
+     libloc_core \
+     libgnss
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
